0c4ef234d7fbf99ab811c8b23d5631ed3234bf16
Patrick BIYAGA <biyagapatrick@gmail.com>
2019-12-10 17:29:43 +0100

    first commit ok

--- backend/database/db.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-	database: 'mongodb+srv://biyaga_admin:biyaga_admin@nodeapi-autrq.mongodb.net/ecommerce?retryWrites=true&w=majority',
-	secretKey: "Patrick@$@!#@"
-}
--- backend/database/keys_dev.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  mongoURI: 'mongodb+srv://biyaga_admin:biyaga_admin@nodeapi-autrq.mongodb.net/ecommerce?retryWrites=true&w=majority',
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  secretOrKey: 'Patrick@$@!#@'
-};
